a5b196fe0ab65557039bcb7218e068d65f1038e8
Yuicihro Kinoshita <kinoshita062323@gmail.com>
2021-09-06 17:11:59 +0000

    [change]

--- app/Models/TopicTemplate.php
+++ app/Models/TopicTemplate.php
@@ -4,8 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\App;
+use App\Models\Input;
 
 class TopicTemplate extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['name', 'description'];
+
+    public function inputs()
+    {
+        return $this->hasMany(Input::class, 'topic_template_id');
+    }
+
+    public function app()
+    {
+        return $this->belongsTo(App::class, 'app_id');
+    }
 }
