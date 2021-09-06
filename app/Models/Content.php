eeb0a27e349048d40a5111b467774865bb489bfd
Yuicihro Kinoshita <kinoshita062323@gmail.com>
2021-09-06 16:56:48 +0000

    [change]

--- app/Models/Content.php
+++ app/Models/Content.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Validation\Rule;
+use App\Models\Topic;
+
 
 class Content extends Model
 {
     use HasFactory;
+
+    protected function rules()
+    {
+        return [
+            'type' => ['required', Rule::in(['multiline', 'singleline'])],
+            'text' => ['required']
+        ];
+    }
+
+    public function topic()
+    {
+        return $this->belongsTo(Topic::class, 'topic_id');
+    }
+
 }
