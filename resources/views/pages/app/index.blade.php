3daff01523e7bc82effb6ab2f82ea2e86fc5aae3
Yuicihro Kinoshita <kinoshita062323@gmail.com>
2021-09-06 18:20:08 +0000

    [add] controller

---
+++ resources/views/pages/app/index.blade.php
@@ -0,0 +1,21 @@
+@extends('adminlte::page')
+
+@section('title', 'アプリ・サービス一覧')
+
+@section('content_header')
+    <h1>アプリ・サービス一覧</h1>
+@stop
+
+@section('content')
+    <p>ここがコンテンツ部分です</p>
+@stop
+
+@section('css')
+    {{-- ページごとCSSの指定
+    <link rel="stylesheet" href="/css/xxx.css">
+    --}}
+@stop
+
+@section('js')
+    <script> console.log('ページごとJSの記述'); </script>
+@stop
