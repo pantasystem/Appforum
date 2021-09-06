fe3d2dec5e50f951db6eb91c053fecb4ee479557
Yuicihro Kinoshita <kinoshita062323@gmail.com>
2021-09-06 17:01:19 +0000

    [change]

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,8 +4,33 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Topic;
+use App\Models\User;
+
 
 class Post extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['text'];
+    
+    public function topic()
+    {
+        return $this->belongsTo(Topic::class, 'topic_id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function replyTo()
+    {
+        return $this->belongsTo(Post::class, 'parent_id');
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Post::class, 'parent_id');
+    }
 }
