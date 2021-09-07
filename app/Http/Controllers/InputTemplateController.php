16d4bc92948e89b48d6e2f58e33a1eeaaf43febd
Yuicihro Kinoshita <kinoshita062323@gmail.com>
2021-09-07 07:45:08 +0000

    [change]

--- app/Http/Controllers/InputTemplateController.php
+++ app/Http/Controllers/InputTemplateController.php
@@ -4,13 +4,23 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\CreateInputTemplateRequest;
 
 
 class InputTemplateController extends Controller
 {
-    public function store()
+    public function store($appId, $templateId, CreateInputTemplateRequest $request)
     {
-        echo 'hogehoge';
+        $user = Auth::user();
+        $app = $user->apps()->findOrFail($appId);
+        $template = $app->topicTemplates()->findOrFail($templateId);
+        $template->inputs()->create(array_merge(
+            $request->only('name', 'description', 'type'),
+            [
+                'is_required' => $request->input('is_required') != 0
+            ]
+        ));
+        return redirect()->route('apps.topic-templates.edit', ['appId' => $app->id, 'templateId' => $template->id]);
     }
 
     public function create($appId, $templateId)
--- resources/views/pages/input_templates/create.blade.php
+++ resources/views/pages/input_templates/create.blade.php
@@ -21,27 +21,49 @@
         <div class="card">
             <div class="card-header">
                 <label for="topic-templates-input-0-name">入力名</label>
-                <input type="text" class="form-control @error('name') is-invalid @enderror" id="topic-templates-input-0-name">
+                <input type="text" class="form-control @error('name') is-invalid @enderror" id="topic-templates-input-0-name" name="name" value="{{old('name')}}">
+                @error('name')
+                <div class='invalid-feedback'>
+                    {{$message }}
+                </div>  
+                @enderror
             </div>
             <div class="card-body">
                 <div class="form-group">
                     <label for="topic-templates-input-0-description">入力についての説明</label>
-                    <textarea class="form-control"></textarea>
+                    <textarea class="form-control" name="description">{{old('description')}}</textarea>
 
                 </div>
                 <div class="form-group">
-                    <label for="topic-templates-input-0-type">入力タイプ</label>
-                    <select class="custom-select">
-                        <option value="singleline">単数行入力</option>
-                        <option value="multiline">複数行入力</option>
-                    </select>
+                    <label for="topic-templates-input-type">入力タイプ</label>
+                    {{ 
+                        Form::select(
+                            'type', 
+                            [
+                                'singleline' => '単数行入力', 
+                                'multiline' => '複数業入力'
+                            ],
+                            old('type'),
+                            [
+                                'class' => 'custom-select',
+                                'id' => 'topic-templates-input-type'
+                            ]
+                        )
+                    }}
+                    @error('type')
+                    <div class='invalid-feedback'>{{$message}}</div>
+                    @enderror
                 </div>
                 <div clsas="form-group">
+
                     <div class="form-check">
-                        <input class="form-check-input" type="checkbox" value="" id="flexCheckDefault">
+                        {{ Form::checkbox('is_required', old('is_required', true), old('is_required', true), ['class' => 'form-check-input']) }}
                         <label class="form-check-label" for="flexCheckDefault">
-                            Default checkbox
+                            必須項目
                         </label>
+                        @error('is_required')
+                        {{$message}}
+                        @enderror
                             
                     </div>
                 </div>
