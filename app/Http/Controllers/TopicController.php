72405ef245f4e0e1d8b748fa08d6f2440bbe9a06
Yuicihro Kinoshita <kinoshita062323@gmail.com>
2021-09-07 12:08:29 +0000

    [change]

---
+++ app/Http/Controllers/TopicController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\App;
+
+class TopicController extends Controller
+{
+    
+    
+    public function store($appId)
+    {
+
+    }
+
+
+    public function create($appId, Request $request)
+    {
+        $app = App::findOrFail($appId);
+        $templateId = $request->input('templateId');
+        if($templateId) {
+            return redirect()->route('apps.topic-templates', ['appId' => $appId]);
+        }
+
+        $template = $app->topicTemplates()->where('is_draft', false)->findOrFail($templateId);
+        return view('pages.topic.create', compact('template'));
+    }
+}
