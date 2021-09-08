d392d645a735fb9d6bccb4640cc73a890ec53306
hanezumi <kanik1580@gmail.com>
2021-09-08 14:23:38 +0900

    Topic一覧途中

--- app/Http/Controllers/TopiclistContrller.php
+++ app/Http/Controllers/TopiclistContrller.php
@@ -5,17 +5,18 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use App\Models\User;
 use App\Models\Content;
 use App\Models\Topic;
+use App\Models\App;
 use Illuminate\Support\Facades\Auth;
 
 class TopiclistContrller extends Controller
 {
-    function index(){
+    function index( App $app_id ){
         //投稿一覧画面の表示
+
         //appsテーブルに対してselect文を実行し、データを取得 id降順
-        $topics = Topic::orderBy('id', 'desc') -> get();
+        $topics = Topic::where('app_id',$app_id)->orderBy('id', 'desc') -> get();
 
         return view('topic', ['data' => $topics]);
     }
