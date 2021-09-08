f111f0516ba273a3f3ad1674961658ae11d850a2
Yu357 <forestgreen14689@gmail.com>
2021-09-08 13:49:27 +0900

    Appモデルをインスタンス化せずに記述しました。 変数dataをappsに改名しました。 アロー演算子の前後の空白を無くしました。

--- app/Http/Controllers/AppController.php
+++ app/Http/Controllers/AppController.php
@@ -7,17 +7,13 @@ use App\Models\App;
 
 class AppController extends Controller
 {
-    //
 
     public function index() {
 
-        //モデルをインスタンス化
-        $app = new App;
-
         //appsテーブルに対してselect文を実行し、データを取得 id降順
-        $data = $app -> orderBy('id', 'desc') -> get();
+        $apps = App::orderBy('id', 'desc')->get();
         
         //bladeテンプレートを呼び出し、データを渡す
-        return view('pages.app.index',['data' => $data]);
+        return view('pages.app.index',['data'=>$apps]);
     }
 }
