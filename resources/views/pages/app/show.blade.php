892ef0cfc89597403276d332de28b4438862b646
Yu357 <forestgreen14689@gmail.com>
2021-09-08 14:13:09 +0900

    新たなbladeファイルを作成

---
+++ resources/views/pages/app/show.blade.php
@@ -0,0 +1,25 @@
+@extends('adminlte::page')
+
+@section('title', 'アプリ・サービス詳細)
+
+@section('content_header')
+    <h1>アプリ・サービス詳細/h1>
+@stop
+
+@section('content')
+    <div>
+
+        <p>ここにアプリ・サービスの詳細を表示する</p>
+
+    </div>
+@stop
+
+@section('css')
+    {{-- ページごとCSSの指定
+    <link rel="stylesheet" href="/css/xxx.css">
+    --}}
+@stop
+
+@section('js')
+    <script> console.log('ページごとJSの記述'); </script>
+@stop
