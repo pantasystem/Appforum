772e85340bbd3fb83d2981032b269d197bad4253
hanezumi <kanik1580@gmail.com>
2021-09-08 14:35:47 +0900

    Topic一覧完成(仮)

--- app/Http/Controllers/TopiclistContrller.php
+++ app/Http/Controllers/TopiclistContrller.php
@@ -12,11 +12,11 @@ use Illuminate\Support\Facades\Auth;
 
 class TopiclistContrller extends Controller
 {
-    function index( App $app_id ){
+    function index( App $app ){
         //投稿一覧画面の表示
 
-        //appsテーブルに対してselect文を実行し、データを取得 id降順
-        $topics = Topic::where('app_id',$app_id)->orderBy('id', 'desc') -> get();
+        //topicsテーブルに対してselect文を実行し、データを取得 id昇順
+        $topics = Topic::where('app_id',$app->id)->orderBy('id', 'asc') -> get();
 
         return view('topic', ['data' => $topics]);
     }
