53c0d82710a1737b71562e5649532d8f1a9b6171
Yu357 <forestgreen14689@gmail.com>
2021-09-08 16:25:57 +0900

    ヘッダー画像とアイコンとアプリ名をそれっぽく配置できた。

--- resources/views/pages/app/show.blade.php
+++ resources/views/pages/app/show.blade.php
@@ -3,19 +3,30 @@
 @section('title', 'アプリ・サービス詳細')
 
 @section('content_header')
-    <h1>{{$app->name}}</h1>
+    <!-- nothing -->
 @stop
 
 @section('content')
     <div>
 
-        <p class="h2"></p>
-        <img src="{{$app->icon_path}}" width="200px">
-        <p>アプリID : {{$app->id}}</p>
-        <p>作成者 : {{$app->user_id}}</p>
-        <p>作成日 : {{$app->created_at}}</p>
-        <p>更新日: {{$app->updated_at}}</p>
-        <p>{{$app->text}}</p>
+        <img src="{{$app->header_image_path}}" class="position-relative w-100" style="height: 300px;">
+
+        <div class="row position-absolute" style="top: 270px;">
+            <img src="/assets/app_icon_skyblue.png" class="img-rounded ml-4" width="200" height="200">
+            <p class="h1 mt-auto ml-3">{{$app->name}}</p>
+        </div>
+
+        
+
+        <div class="border border-secondary" style="margin-top: 90px">
+            <p class="h3">アプリ詳細</p>
+            <p>アプリID : {{$app->id}}</p>
+            <p>作成者 : {{$app->user_id}}</p>
+            <p>作成日 : {{$app->created_at}}</p>
+            <p>更新日: {{$app->updated_at}}</p>
+            <p>{{$app->text}}</p>
+        </div>
+
 
     </div>
 @stop
