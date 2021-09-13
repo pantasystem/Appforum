76f33483b1c6566c878527ef5c6586e246e38678
Yu357 <forestgreen14689@gmail.com>
2021-09-13 15:33:47 +0900

    CSSのfloatプロパティをBootstrapで記述した。

--- resources/views/components/app-header.blade.php
+++ resources/views/components/app-header.blade.php
@@ -6,7 +6,7 @@
 </div>
 
 <div class="w-100" style="margin-bottom: 80px;">
-    <a href="{{ route('apps.topics.create', ['appId' => $app->id]) }}" class="btn btn-primary mt-3 mr-5" style="float: right;">トピック作成</a>
+    <a href="{{ route('apps.topics.create', ['appId' => $app->id]) }}" class="btn btn-primary mt-3 mr-5 float-right">トピック作成</a>
 </div>
 
 </div>
