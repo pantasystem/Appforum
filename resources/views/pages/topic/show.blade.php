01495782bcb6c620ad41b05103b2ea0e38e2fb90
Yu357 <forestgreen14689@gmail.com>
2021-09-13 16:38:10 +0900

    トピック画面にぱんくずリストを実装した

--- resources/views/pages/topic/show.blade.php
+++ resources/views/pages/topic/show.blade.php
@@ -3,6 +3,16 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
+
+    <nav area-label="breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="{{ route('apps.index')}}">アプリ・サービス一覧</a></li>
+            <li class="breadcrumb-item"><a href="{{route('apps.topic-templates.create', ['appId' => $app->id])}}">{{ $app->name}}</a></li>
+            <li class="breadcrumb-item"><a href="{{route('apps.topic.index', ['app' => $app->id])}}">トピック一覧</a></li>
+            <li class="breadcrumb-item active" aria-current="page">{{ $topic->title }}</li>   
+        </ol>
+    </nav>
+    
     <div>
         <h1>{{ $topic->title }}</h1>
         <!-- TODO breadcrumbを表示する -->
