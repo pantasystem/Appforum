cbe6fa2c47a5c798a3d70882d96047add5eaf930
Yuicihro Kinoshita <kinoshita062323@gmail.com>
2021-09-13 06:04:35 +0000

    [add] create migration and model

---
+++ database/migrations/2021_09_13_054050_create_stamps_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStampsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stamps', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('name')->unique();
+            $table->string('image')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stamps');
+    }
+}
---
+++ database/migrations/2021_09_13_054107_create_topic_reactions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTopicReactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('topic_reactions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('stamp_id');
+            $table->unsignedBigInteger('topic_id');
+            $table->foreign('user_id')->references('id')->on('users');
+            $table->foreign('stamp_id')->references('id')->on('stamps');
+            $table->foreign('topic_id')->references('id')->on('topics');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('topic_reactions');
+    }
+}
