a194b1ba99a2261dc0935bf1c3010653d0199d37
Yuicihro Kinoshita <kinoshita062323@gmail.com>
2021-09-13 16:26:40 +0000

    [change]

--- resources/views/pages/post/index.blade.php
+++ resources/views/pages/post/index.blade.php
@@ -8,10 +8,14 @@
 
 @section('content')
 <div>
-    <x-post-view :post="$replyTo" :topic="$topic" :app="$app"/>
+    <div class="post-view-{{$replyTo->id}}">
+        <x-post-view :post="$replyTo" :topic="$topic" :app="$app"/>
+    </div>
     <div class="pl-5">
         @foreach($replies as $reply)
+            <div class="post-view-{{$reply->id}}">
             <x-post-view :post="$reply" :topic="$topic" :app="$app" />
+            </div>
         @endforeach
         <x-post-editor-form :replyTo="$replyTo" :topic="$topic"/>
     </div>
@@ -22,5 +26,49 @@
 @stop
 
 @section('js')
-    <script> console.log('ページごとJSの記述'); </script>
+@section('js')
+<script>
+const topicId = {{$topic->id}};
+const appId = {{$app->id}};
+
+function setupReactionCounters(element) {
+    const reactionCounters = element.getElementsByClassName('reaction-counter');
+
+    for(let i = 0; i < reactionCounters.length; i ++) {
+        const reactionCounter = reactionCounters[i];
+        reactionCounter.addEventListener('click', onReactionClickedListener);
+    }
+}
+
+const onReactionClickedListener = function(e) {
+    const stampId = e.target.getAttribute('data-stamp-id');
+    const postId = e.target.getAttribute('data-post-id');
+    const path = `/apps/${appId}/topics/${topicId}/posts/${postId}/reactions`;
+    const token = document.getElementsByName('csrf-token').item(0).content; 
+    $.ajax({
+        type: 'POST',
+        url: path,
+        data: {
+            'stamp_id': stampId
+        },
+        headers: {
+            'X-CSRF-TOKEN': token,
+        }
+    }).done((res)=>{
+        //document.documentElement.innerHTML = res;
+        const postElements = document.getElementsByClassName(`post-view-${postId}`);
+        console.log(postElements);
+        for(let i = 0; i < postElements.length; i ++) {
+            postElements[i].innerHTML = res;
+            setupReactionCounters(postElements[i]);
+        }
+    });       
+}
+
+setupReactionCounters(document);
+
+
+
+</script>
+@endsection
 @stop
