103d2a7218d417114b0c1bb92e3ac1cd92497c61
Yu357 <forestgreen14689@gmail.com>
2021-09-14 15:14:14 +0900

    コードを簡略化

--- resources/views/components/app-header.blade.php
+++ resources/views/components/app-header.blade.php
@@ -1,13 +1,9 @@
-<div>
+<div class="w-100 position-relative" style="background-image: url('{{$app->header_image_url}}'); height: 300px; margin-bottom: 70px;">
 
-    <div class="w-100 position-relative" style="background-image: url('{{$app->header_image_url}}'); height: 300px; margin-bottom: 70px;">
-
-        <div class="position-absolute row w-100" style="bottom: -60px;">
-            <img src="{{$app->icon_url}}" class="img-rounded bg-white ml-4" width="150" height="150">
-            <h1 class="ml-3 mt-auto mb-0">{{$app->name}}</h1>
-            <a href="{{ route('apps.topics.create', ['appId' => $app->id]) }}" class="btn btn-primary ml-auto mt-auto">トピック作成</a>
-        </div>
-
+    <div class="position-absolute row w-100" style="bottom: -60px;">
+        <img src="{{$app->icon_url}}" class="img-rounded bg-white ml-4" width="150" height="150">
+        <h1 class="ml-3 mt-auto mb-0">{{$app->name}}</h1>
+        <a href="{{ route('apps.topics.create', ['appId' => $app->id]) }}" class="btn btn-primary ml-auto mt-auto">トピック作成</a>
     </div>
 
 </div>
