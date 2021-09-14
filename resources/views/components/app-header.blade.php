474766870e8e686af48ec6c09a477a5ecff4496c
Yu357 <forestgreen14689@gmail.com>
2021-09-14 15:04:01 +0900

    一旦コミット

--- resources/views/components/app-header.blade.php
+++ resources/views/components/app-header.blade.php
@@ -1,12 +1,27 @@
 <div>
-<img src="{{$app->header_image_url}}" class="position-relative w-100" style="height: 300px;">
-<div class="row position-absolute" style="top: 320px;">
-    <img src="{{$app->icon_url}}" class="img-rounded bg-white ml-4" width="150" height="150">
-    <h1 class="mt-auto ml-3 mb-0">{{$app->name}}</h1>
+
+<div class="w-100 position-relative" style="background-image: url('{{$app->header_image_url}}'); height: 300px; margin-bottom: 60px;">
+
+    <div class="position-absolute row w-100" style="bottom: -50px;">
+        <img src="{{$app->icon_url}}" class="img-rounded bg-white ml-4" width="150" height="150">
+        <h1 class="mt-auto ml-3 mb-0">{{$app->name}}</h1>
+        <a href="{{ route('apps.topics.create', ['appId' => $app->id]) }}" class="btn btn-primary mt-3 mr-5 ml-auto">トピック作成</a>
+    </div>
+
 </div>
 
-<div class="w-100" style="margin-bottom: 80px;">
+
+
+<!-- <img src="{{$app->header_image_url}}" class="position-relative w-100" style="height: 300px;"> -->
+
+
+<!-- <div class="row position-absolute" style="top: 320px;">
+    <img src="{{$app->icon_url}}" class="img-rounded bg-white ml-4" width="150" height="150">
+    <h1 class="mt-auto ml-3 mb-0">{{$app->name}}</h1>
+</div> -->
+
+<!-- <div class="w-100" style="margin-bottom: 80px;">
     <a href="{{ route('apps.topics.create', ['appId' => $app->id]) }}" class="btn btn-primary mt-3 mr-5 float-right">トピック作成</a>
-</div>
+</div> -->
 
 </div>
