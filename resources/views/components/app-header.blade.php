f3b18e92fe14794dc4428f299af30dfd6b9e6dd7
Yu357 <forestgreen14689@gmail.com>
2021-09-14 15:17:32 +0900

    今後のボタン追加を想定して、トピック作成ボタンをdivで囲んだ

--- resources/views/components/app-header.blade.php
+++ resources/views/components/app-header.blade.php
@@ -3,7 +3,9 @@
     <div class="position-absolute row w-100" style="bottom: -60px;">
         <img src="{{$app->icon_url}}" class="img-rounded bg-white ml-4" width="150" height="150">
         <h1 class="ml-3 mt-auto mb-0">{{$app->name}}</h1>
-        <a href="{{ route('apps.topics.create', ['appId' => $app->id]) }}" class="btn btn-primary ml-auto mt-auto">トピック作成</a>
+        <div class="ml-auto mt-auto">
+            <a href="{{ route('apps.topics.create', ['appId' => $app->id]) }}" class="btn btn-primary ">トピック作成</a>
+        </div>
     </div>
 
 </div>
