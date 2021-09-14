008ef4f955315408fc0ae5eb5f420cb85f31b864
Yu357 <forestgreen14689@gmail.com>
2021-09-14 15:41:22 +0900

    ヘッダー画像が左上が拡大されて表示される不具合を修正した

--- resources/views/components/app-header.blade.php
+++ resources/views/components/app-header.blade.php
@@ -1,6 +1,6 @@
-<div class="w-100 position-relative" style="background-image: url('{{$app->header_image_url}}'); height: 300px; margin-bottom: 70px;">
+<div class="w-100 position-relative" style="background-image: url('{{$app->header_image_url}}'); background-position:center; background-size:cover; height:300px; margin-bottom:70px;">
 
-    <div class="position-absolute row w-100" style="bottom: -60px;">
+    <div class="position-absolute row w-100" style="bottom:-60px;">
         <img src="{{$app->icon_url}}" class="img-rounded bg-white ml-4" width="150" height="150">
         <h1 class="ml-3 mt-auto mb-0">{{$app->name}}</h1>
         <div class="ml-auto mt-auto">
